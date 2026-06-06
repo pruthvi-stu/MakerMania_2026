94914ae56d1c16b3930e895e8bc8510aeb17d5df
spidyy19 <jadhavpruthvi826@gmail.com>
2026-06-06 15:04:07 +0530

    Update testing.c to contain only the video link

--- Training Material/testing.c
+++ Training Material/testing.c
@@ -1,14 +1,3 @@
-// Training Material - testing.c
-// Reference video:
-// https://youtu.be/VmvSfJyc5I4?si=qANdnrm9vCCbVehJ
 
-/*
-  This file contains a reference to the MakerMania demo video.
-  Add any test code or notes below as needed.
-*/
-
-int main(void) {
-    // Placeholder test harness
-    return 0;
-}
+https://youtu.be/VmvSfJyc5I4?si=qANdnrm9vCCbVehJ
 
